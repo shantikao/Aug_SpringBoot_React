2b1be6ed9b75285ff6e573ba130967d59e71fbaa
shanti <shanti@cht.com.tw>
2020-08-11 16:50:00 +0800

    change async callback to arrow function

--- lab46_async_property.js
+++ lab46_async_property.js
@@ -1,15 +1,80 @@
 
+// const fs = require('fs')
+// function FileObject() {
+//     this.filename = ''
+//     this.file_exists = function (callback) {
+//         console.log("about to open:" + this.filename)
+
+//     }
+// }
+
+// var fo1 = new FileObject()
+// fo1.filename = "README.md"
+// fo1.file_exists((error, results) => {
+//     if (error) {
+//         console.log("error open file:" + error)
+//         return
+//     }
+//     console.log("file exists!", results)
+// })
+// ~~~~~~~~~~~~~~~~~~~~~~~~~
+// const fs = require('fs')
+// function FileObject() {
+//     this.filename = ''
+//     this.file_exists = function (callback) {
+//         const that = this
+//         console.log("what is this?" + this, this.filename)
+//         console.log("about to open:" + this.filename)
+//         fs.open(this.filename, 'r', function (error, handler) {
+//             console.log("what is this?" + that, that.filename)
+//             if (error) {
+//                 console.log("can not open:" + that.filename)
+//                 callback(error)
+//                 return
+//             }
+//             console.log("the file we process is:" + that.filename)
+//             console.log("now we can process file with handler" + handler)
+//             fs.close(handler, function () { })
+//             callback(null, true);
+//         })
+
+//     }
+// }
+
+// var fo1 = new FileObject()
+// fo1.filename = "README.mdx"
+// fo1.file_exists((error, results) => {
+//     if (error) {
+//         console.log("error open file:" + error)
+//         return
+//     }
+//     console.log("file exists!", results)
+// })
+// ~~~~~~~~~~~~~~~~~~
 const fs = require('fs')
 function FileObject() {
     this.filename = ''
     this.file_exists = function (callback) {
+        console.log("what is this?" + this, this.filename)
         console.log("about to open:" + this.filename)
+        fs.open(this.filename, 'r', (error, handler) => {
+            console.log("what is this?" + this, this.filename)
+            if (error) {
+                console.log("can not open:" + this.filename)
+                callback(error)
+                return
+            }
+            console.log("the file we process is:" + this.filename)
+            console.log("now we can process file with handler" + handler)
+            fs.close(handler, function () { })
+            callback(null, true);
+        })
 
     }
 }
 
 var fo1 = new FileObject()
-fo1.filename = "README.md"
+fo1.filename = "README.mdx"
 fo1.file_exists((error, results) => {
     if (error) {
         console.log("error open file:" + error)
