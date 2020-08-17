19e0969c679b29c5afb1608669a69b9cc5f7b744
shanti <shanti@cht.com.tw>
2020-08-17 13:44:54 +0800

    add .bablerc to enable bable interpretation

---
+++ Lab2_babel_modern_js_syntax/src/lab1_var_let_const.js
@@ -0,0 +1,13 @@
+
+let x = 5
+console.log(x)
+x += 1
+console.log(x)
+
+const y = `hello world, actually x=${x}`
+console.log(y)
+const z = `hihi, x=${x}`
+console.log(z)
+
+
+//npx babel src\lab1_var_let_const.js
