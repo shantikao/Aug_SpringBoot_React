b58db68bca51f63728e674aa4c265efad38ed50d
shanti <shanti@cht.com.tw>
2020-08-17 15:23:33 +0800

    class with babel plugin

--- Lab2_babel_modern_js_syntax/src/lab4_class2.js
+++ Lab2_babel_modern_js_syntax/src/lab4_class2.js
@@ -1,7 +1,16 @@
 class Course {
     duration = 49
     printDuration = () => {
-        console.log(`duration = ${this.duration}`)
+        return `duration = ${this.duration}`
     }
 }
-const c1 = new Course()
+class ReactCourse extends Course {
+    name = 'React'
+    printCourse = () => {
+        return `course name = ${this.name}`
+    }
+}
+const c1 = new ReactCourse()
+console.log(`c1 course ${c1.printDuration()}, ${c1.printCourse()}`)
+
+//npm install @babel/plugin-proposal-class-properties
