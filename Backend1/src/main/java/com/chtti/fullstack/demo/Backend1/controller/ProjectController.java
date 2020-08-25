2bae83d7e2d4b0336d17d4148cd48d4d79faea53
shanti <shanti@cht.com.tw>
2020-08-25 14:38:15 +0800

    add cross origin resource sharing for ProjectController

--- Backend1/src/main/java/com/chtti/fullstack/demo/Backend1/controller/ProjectController.java
+++ Backend1/src/main/java/com/chtti/fullstack/demo/Backend1/controller/ProjectController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.Map;
 @RestController
+@CrossOrigin
 @RequestMapping("/api/project")
 public class ProjectController {
     @Autowired
